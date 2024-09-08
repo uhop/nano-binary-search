8f39ea5330ab3dc00d408233e0337bcb1b7088c8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-08 10:38:34 -0500

    Modified type checks.

--- ts-check/check.ts
+++ ts-check/check.ts
@@ -1,7 +1,8 @@
 import binarySearch from 'nano-binary-search';
 
-const a: number = binarySearch([1, 2, 3, 4, 5], x => x < 3);
-const b: number = binarySearch([1, 2, 3, 4, 5], x => x < 3, 1, 3);
-const c: number = binarySearch([...'12345'], x => x < '3');
-const d: number = binarySearch<number | string>([...'12345'], x => +x < 3);
-const e: number = binarySearch<number | string>([...'12345'], x => String(x) < '3');
+const a = binarySearch([1, 2, 3, 4, 5], x => x < 3);
+const b = binarySearch([1, 2, 3, 4, 5], x => x < 3, 1, 3);
+const c = binarySearch([...'12345'], x => x < '3');
+const d = binarySearch([...'12345'], x => +x < 3);
+const e = binarySearch([1, '2', 3], x => +x < 3);
+const f = binarySearch([1, '2', 3], x => String(x) < '3');
